8a4bb469c4e1ab9a9d681620447b7220f27d835c
Zubi-Duru <duruazubuike2019@gmail.com>
2021-05-02 09:55:41 -0700

    added my old codes

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// let heading = document.querySelector("h1");
+// let body = document.querySelector("body");
+
+// var myColors = ["red","purple","yellow","pink"];
+
+// heading.addEventListener("mouseover",function zubi(){
+//    for(var i = 0; i < myColors.length; i++){
+//     body.style.background = myColors[i];
+//    };
+  
+// });
+
+
+
+//  function infor(name){
+//    let fname;
+//     if (name === "duru"){
+//        fname = true;
+//     }
+//     return fname;
+//  };
+
+//  infor(duru);
